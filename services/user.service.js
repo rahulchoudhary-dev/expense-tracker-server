9c64cc58367325f309e780983fffaf4fc1c6cee4
Rahul <rahul.choudhary@gmail.com>
2025-07-15 23:00:27 +0530

    feat: updated user service and api

--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,29 @@
 const { Sequelize } = require("sequelize");
 const User = require("../models/user.js");
+const handleSequelizeError = require("../utils/sequelizeErrorHandler.js");
 
-const userService = {};
+const userService = {
+  updateUser: async (userId, userData) => {
+    try {
+      delete userData.password;
+      delete userData.confirmPassword;
+      delete userData.id;
+
+      const user = await User.findOne({ where: { id: userId } });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      const resp = await User.update(userData, {
+        where: { id: userId },
+        returning: true,
+      });
+      const updatedUser = resp[1][0];
+
+      return updatedUser;
+    } catch (error) {
+      throw handleSequelizeError(error);
+    }
+  },
+};
 
 module.exports = userService;
